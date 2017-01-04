b682118bb32332f23ed4cba34a798b28290a6b99
patrice prosper <new>
2017-01-04 11:36:11 -0500

    Set up XHR request to server

---
+++ Pokeapp/pokeApi.js
@@ -0,0 +1,31 @@
+let count = 1;
+
+function getData(num) {
+  $.get(`https://pokeapi.co/api/v1/pokemon/${num}/`, function(data) {
+   /* $('#dataview').html('<p> Name: ' + data.name + '</p>');
+     $('#dataview').append('<p>Eye Color : ' + data.eye_color + '</p>');
+     $('#dataview').append('<p> Gender: ' + data.gender + '</p>');
+    $('#dataview').html('');*/
+    $.each(data, function(key, value) {
+      $('#dataview').append(`<p><strong>${key}</strong>: ${value}</p>`);
+    });
+    $('#counter').text(`Person: ${count}`);
+    console.log(data)
+  });
+}
+
+
+
+$(function() {
+  getData(count);
+
+  $('#next').on('click', function() {
+    getData(++count);
+  });
+
+  $('#prev').on('click', function() {
+    if(count > 1) getData(--count);
+  });
+
+
+});
