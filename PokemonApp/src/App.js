02657c38f74d49e26b5b9b373e649c338d480e50
patrice prosper <new>
2017-01-08 20:34:28 -0500

    got Pokemon list of 151 names to display and details to show when name clicked

--- PokemonApp/src/App.js
+++ PokemonApp/src/App.js
@@ -1,58 +1,74 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import $ from 'jquery';
 import '../css/App.css';
 
 class App extends Component {
-constructor () {
-super ();
-this.state = {
-  name:'name',
-  weight:'weight',
-};
-/*this.getPokemon = this.getPokemon.bind(this);
-this.renderPokeData = this.renderPokeData.bind(this);*/
+constructor(props) {
+  super(props);
+  this.state = {
+    pokemon: [],
+  };
+}
+
+componentDidMount() {
+  $.ajax({
+    url: "https://pokeapi.co/api/v1/pokemon?limit=151&offset=1",
+    method: "GET"
+  }).done((response)=>{
+    let pokemon = response.objects;
+    this.setState({pokemon: pokemon});
+    console.log(pokemon);
+  });
+}
+
+  // use the index to make another api call to get more data about the planet
+  // do that by looking up the planets id and then adding it to the url
+  // i.e. '/plantes/:id'
+  //
+  // make an api call and set a 'currentPlanet' property in our state
+  // add a ui to render that currentPlanets details
+  //
+  // OR
+  //
+  // use info in planets array and don't make another call
+
+selectPokemon(index) {
+  this.setState({ currentPokemon: this.state.pokemon[index] });
 }
 
 
-getPokemon() {
-  axios.get('https://pokeapi.co/api/v1/pokemon/').then ((response)=> {
-  this.renderPokeData(response.data.objects.name, response.data.objects.weight)
-  })
-   .catch((err) => { console.error(err); });
-}
+render() {
+  // onClick takes a function and when the element that onClick is
+  // bound to, that function is called.
 
-
-/*renderPokeData(name, weight) {
-    this.setState({
-      name:
-      weight:
-    });
-  }*/
+  let pokeList = this.state.pokemon.map((Pokemon, i) => {
+   return(
+      <li key={i}onClick={ () => { this.selectPokemon(i) } } >{Pokemon.name}</li>);
+ });
 
 
 
-render(){
+ let pokeDetails
+  if (this.state.currentPokemon) {
+   pokeDetails = this.state.currentPokemon.name;
+ } else {
+   pokeDetails = "Select a Pokemon";
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Make Your Pokemon! </h1>
-        <p> </p>
-        <hr />
-      </header>
-      <div className="list">
-      <h3> Choose Your Pokemon:</h3>
-      <p id="name"> {this.state.name} </p>
+      <div className="planets-list-container">
+        <ul className="planets-list">
+        {pokeList}
+        </ul>
       </div>
-
-      <div className="userPoke">
-      <p id="details"> {this.state.weight} </p>
+      <div className="planet-container">
+        {pokeDetails}
       </div>
-      <footer>
-        <hr />
-        Data Served From: <a href="https://pokeapi.co">Pokeapi.co</a>
-      </footer>
     </div>
   );
 }
 }
-export default App
+
+
+export default App;
