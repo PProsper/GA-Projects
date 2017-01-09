fc94b892da1e7b5075dacfb5a70de3e91db34d6d
patrice prosper <new>
2017-01-09 00:51:31 -0500

    added css styling with flex box

--- PokemonApp/src/App.js
+++ PokemonApp/src/App.js
@@ -3,72 +3,63 @@ import $ from 'jquery';
 import '../css/App.css';
 
 class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    pokemon: [],
-  };
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokemon: [],
+        };
+    }
 
-componentDidMount() {
-  $.ajax({
-    url: "https://pokeapi.co/api/v1/pokemon?limit=151&offset=1",
-    method: "GET"
-  }).done((response)=>{
-    let pokemon = response.objects;
-    this.setState({pokemon: pokemon});
-    console.log(pokemon);
-  });
-}
+    componentDidMount() {
+        $.ajax({
+            url: "https://pokeapi.co/api/v1/pokemon?limit=151&offset=1",
+            method: "GET"
+        }).done((response) => {
+            let pokemon = response.objects;
+            //binds variable pokemon to state pokemon not sure which is which...
+            this.setState({ pokemon: pokemon });
+            console.log(response);
+            //response is slow possible beacuse of limit amount
+        });
+    }
 
-  // use the index to make another api call to get more data about the planet
-  // do that by looking up the planets id and then adding it to the url
-  // i.e. '/plantes/:id'
-  //
-  // make an api call and set a 'currentPlanet' property in our state
-  // add a ui to render that currentPlanets details
-  //
-  // OR
-  //
-  // use info in planets array and don't make another call
+    //Another api call needs to be made using index
+    // it is setting a 'currentPokemon' property in state
+    // ui is added to render that currentPokemon details
 
-selectPokemon(index) {
-  this.setState({ currentPokemon: this.state.pokemon[index] });
-}
+    selectPokemon(index) {
+        this.setState({ currentPokemon: this.state.pokemon[index] });
+    }
 
 
-render() {
-  // onClick takes a function and when the element that onClick is
-  // bound to, that function is called.
+    render() {
+        // onClick takes a function and when the element that onClick is
+        // bound to, that function is called.
 
-  let pokeList = this.state.pokemon.map((Pokemon, i) => {
-   return(
-      <li key={i}onClick={ () => { this.selectPokemon(i) } } >{Pokemon.name}</li>);
- });
+        let pokeList = this.state.pokemon.map((Pokemon, i) => {
+                return ( < li key = { i }
+                    onClick = {
+                        () => { this.selectPokemon(i) } } > { Pokemon.name } < /li>);
+                });
 
 
 
- let pokeDetails
-  if (this.state.currentPokemon) {
-   pokeDetails = this.state.currentPokemon.name;
- } else {
-   pokeDetails = "Select a Pokemon";
- }
+            let pokeName
+            let pokeWeight
+            if (this.state.currentPokemon) {
+                pokeName = 'Name: ' + this.state.currentPokemon.name;
+                pokeWeight = 'Weight: ' + this.state.currentPokemon.weight;
+            } else {
+                pokeName = "Select a Pokemon";
+            }
 
-  return (
-    <div className="App">
-      <div className="planets-list-container">
-        <ul className="planets-list">
-        {pokeList}
-        </ul>
-      </div>
-      <div className="planet-container">
-        {pokeDetails}
-      </div>
-    </div>
-  );
-}
-}
+            return ( < div className = "App" >
+                < h1 > Make Your Own Pokemon < /h1> < div className = "planets-list-container" >
+                < ul className = "list" > { pokeList } < /ul> < /div> < hr / >
+                < div className = "details" > { pokeName } < div className = "weight" > { pokeWeight } < /div> < /div> < /div>
+            );
+        }
+    }
 
 
-export default App;
+    export default App;
