0bda296ecf12c00f5b75b948dbc4c07eef6df7dd
patrice prosper <new>
2017-01-12 09:07:46 -0500

    setState and reworker method for displaying data

--- Tweedr/src/App.js
+++ Tweedr/src/App.js
@@ -6,21 +6,41 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state={
-      rants:'',
+      rants:[],
     };
+    this.showList = this.showList.bind(this);
   }
 
 componentWillMount(){
     getData().then((res)=> {
-         console.log(res);
-         let rants = res.data;
-         this.setState({rants: rants});
+         console.log(res.data);
+          let rants = res.data;
+        //  console.log(res.Object);
+        //  this.setState({rants: rants});
       });
   }
 
+showList (index) {
+        this.setState({currentRant: this.state.rants[index]});
+    }
+
 render(){
+  let rantsList = this.state.rants.map((Rant, i)=> {
+          return( < li key={ i }
+              onLoad={
+                  ()=>{ this.showList(i)}} > {Rant.data} < /li>);
+          });
+          let ObjList
+          if (this.state.currentRant){
+           ObjList = this.state.currentRant.data;
+           } else {
+           ObjList = "null";
+             }
+
   return(
     <div className="App">
+    < ul className="list"> {rantsList} < /ul>
+      < li className="rant" > {ObjList} < /li>
     </div>
   )
 }
